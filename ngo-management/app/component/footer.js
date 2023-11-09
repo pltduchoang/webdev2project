1a722d0507b34d9e31e53f433fcc9d5981f52c46
pltduchoang <plt.duchoang@gmail.com>
2023-11-09 10:28:26 -0700

    lab3

---
+++ ngo-management/app/component/footer.js
@@ -0,0 +1,9 @@
+export default function Footer () {
+  return (
+    <main className="fixed bottom-0 w-full bg-slate-500">
+      <footer>
+        <p>© 2021 NGO Management</p>
+      </footer>
+    </main>
+  )
+}
---
+++ ngo-management/app/component/heading.js
@@ -0,0 +1,7 @@
+export default function ({title}) {
+    return (
+      <header>
+        <h1>{title}</h1>
+      </header>
+    )
+}
